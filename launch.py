a6314686adc8f377319806d6829e404f949ec303
mohakkatonic <141838247+mohakkatonic@users.noreply.github.com>
2023-11-13 08:46:28 +0530

    Update launch.py

--- launch.py
+++ launch.py
@@ -25,20 +25,7 @@ def preprocessing(data, logger):
     logger.info("Task fetched.")
 
     # Modify the prompt to guide the model in extracting information
-    final_prompt = (
-        f"""
-    You are an NDA (Non-Disclosure Agreement) reader expert, who specializes in reviewing and understanding the intricacies of confidentiality agreements. You possess a deep understanding of legal language and can interpret the terms and conditions outlined in NDAs to ensure clarity and protection of sensitive information for all parties involved. Extract the following information from the given NDA document content:
-    Context: {data['data']}
-    Information to extract: Client Name, Contract Start Date, Contract End Date, Special Terms for Renewal
-    Extract based on below definitions:
-    Client Name: The legal or business name of the party involved in the NDA.
-    Contract Start Date: The specific date when the contractual agreement covered by the NDA begins.
-    Contract End Date: The date marking the termination or expiration of the contractual agreement specified in the NDA.
-    If the contract end date is not specified directly, in such cases, the duration of confidentiality is often addressed in the "Survival" clause. Calculate the end date from there and give output as a date.
-    Special Terms for Renewal: Unique conditions or provisions in the NDA outlining terms for contract renewal.
-    Extracted Information:
-    """
-    )
+    final_prompt = os.environ.get("PROMPT")
 
     logger.info("Created the final Prompt")
     return final_prompt
